3366815ab57d2cbd6e7fe41b27a372b8e8cda639
katsiev1997 <katsiev1997@gmail.com>
2024-04-03 01:55:18 +0300

    fix button CreateReserve

--- src/features/Reserve/ui/ReserveClient.tsx
+++ src/features/Reserve/ui/ReserveClient.tsx
@@ -1,30 +1,28 @@
-import React from 'react';
-import cls from './Reserve.module.scss';
-import { Button, Input, message } from 'antd';
-import { CloseOutlined, PhoneOutlined } from '@ant-design/icons';
-import { times } from '../model/consts/times';
-import { useSelector } from 'react-redux';
-import { StateSchema } from 'src/app/provider/StoreProvider/config/StateSchema';
-import { setTimeReserve } from '..';
-import { useAppDispatch } from 'src/shared/hooks/useAppDispatch';
+import React from "react";
+import cls from "./Reserve.module.scss";
+import { Button, Input, message } from "antd";
+import { CloseOutlined, PhoneOutlined } from "@ant-design/icons";
+import { times } from "../model/consts/times";
+import { useSelector } from "react-redux";
+import { StateSchema } from "src/app/provider/StoreProvider/config/StateSchema";
+import { setTimeReserve } from "..";
+import { useAppDispatch } from "src/shared/hooks/useAppDispatch";
 
 export const ReserveClient: React.FC = () => {
-  const { from, to, room, club } = useSelector(
-    (state: StateSchema) => state.reserve
-  );
-  const dispatch = useAppDispatch()
+  const { from, to, room, club } = useSelector((state: StateSchema) => state.reserve);
+  const dispatch = useAppDispatch();
   const { clubs } = useSelector((state: StateSchema) => state.club);
   const Club = clubs[club];
-  const [customerData, setCustomerData] = React.useState<string>('');
+  const [customerData, setCustomerData] = React.useState<string>("");
   const [messageApi, contextHolder] = message.useMessage();
   const [open, setOpen] = React.useState<boolean>(false);
   const errorMessage = (text: string) => {
     messageApi.open({
-      type: 'error',
+      type: "error",
       content: text,
       style: {
-        fontSize: '16px',
-        marginTop: '30vh',
+        fontSize: "16px",
+        marginTop: "30vh",
       },
     });
   };
@@ -32,20 +30,24 @@ export const ReserveClient: React.FC = () => {
   // Устанавливаем URL сервера
   const createReserve = () => {
     if (from === null || to === null) {
-      errorMessage('Выберите время бронирования!');
+      errorMessage("Выберите время бронирования!");
       return;
     }
     const setRoom = Club.rooms[room];
     for (let i = from; i <= to; i++) {
       if (!setRoom.availableTimeSlots[i]) {
-        errorMessage('Выбранное время уже забронировано');
-        dispatch(setTimeReserve(100))
-        return
+        errorMessage("Выбранное время уже забронировано");
+        dispatch(setTimeReserve(100));
+        return;
       }
     }
+    window.location.href = `https://wa.me/+7${
+      Club.phone
+    }?text=Бронь Телефон: ${customerData} Клуб: ${Club.name} Комната №: ${room + 1} С ${
+      from && times[from].label
+    } до ${to && times[to + 1].label}`;
     setOpen(true);
   };
-
 
   return (
     <React.Fragment>
@@ -53,22 +55,18 @@ export const ReserveClient: React.FC = () => {
       <button onClick={() => createReserve()}>
         <img
           className={cls.booking}
-          src='https://img.icons8.com/?size=160&id=79996&format=png'
-          alt=''
+          src="https://img.icons8.com/?size=160&id=79996&format=png"
+          alt=""
         />
         <h4>Reserve</h4>
       </button>
-      <div
-        className={
-          open ? `${cls.create_booking} ${cls.active}` : cls.create_booking
-        }
-      >
+      <div className={open ? `${cls.create_booking} ${cls.active}` : cls.create_booking}>
         <CloseOutlined
           style={{
-            fontSize: '40px',
-            position: 'absolute',
-            right: '20px',
-            top: '20px',
+            fontSize: "40px",
+            position: "absolute",
+            right: "20px",
+            top: "20px",
           }}
           onClick={() => setOpen(false)}
         />
@@ -76,13 +74,13 @@ export const ReserveClient: React.FC = () => {
         <Input
           prefix={
             <div>
-              <PhoneOutlined className='site-form-item-icon' /> +7
+              <PhoneOutlined className="site-form-item-icon" /> +7
             </div>
           }
           onChange={(e) => setCustomerData(e.target.value)}
-          size='large'
-          className='input'
-          placeholder='Номер телефона...'
+          size="large"
+          className="input"
+          placeholder="Номер телефона..."
         />
         <h4>Комната: {room + 1}</h4>
         {from !== null && to !== null ? (
@@ -94,21 +92,11 @@ export const ReserveClient: React.FC = () => {
         )}
         <Button
           onClick={createReserve}
-          size='large'
-          type='primary'
-          style={{ marginTop: '20px' }}
+          size="large"
+          type="primary"
+          style={{ marginTop: "20px" }}
         >
-          <a
-            href={`https://wa.me/+7${
-              Club.phone
-            }?text=Бронь Телефон: ${customerData} Клуб: ${
-              Club.name
-            } Комната №: ${room + 1} С ${from && times[from].label} до ${
-              to && times[to + 1].label
-            }`}
-          >
-            Забронировать
-          </a>
+          Забронировать
         </Button>
       </div>
     </React.Fragment>
